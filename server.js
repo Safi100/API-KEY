18a7d80d67840c33fe0b473bc80c12fc99809c3b
safi100 <safinafi12@gmail.com>
2024-04-07 01:03:22 +0200

    post new api key

--- server.js
+++ server.js
@@ -12,10 +12,33 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 
+// models
+const Api_key = require('./models/api_key');
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.post('/generate-api-key', async (req, res) => {
+    try{
+        const { requests_per_month, number_of_months } = req.body;
 
+        if(number_of_months < 1) throw new Error('Number of months must be greater than 0');
+        if(requests_per_month < 1) throw new Error('Number of requests per month must be greater than 0');
+
+        const expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth() + number_of_months);
+
+        const newApiKey = new Api_key({
+            requests_per_month: requests_per_month,
+            expiration_date: expirationDate
+        });
+        await newApiKey.save();
+        res.json(newApiKey);
+    }catch(err){
+        res.status(500).send(err);
+    }
+
+});
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
